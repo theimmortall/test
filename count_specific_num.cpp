b7d9f58d5a962edc357b5da13ec62820fa282308
Divyam Singh <104553605+theimmortall@users.noreply.github.com>
2024-08-01 11:17:53 +0530

    modified:   count_specific_num.cpp

--- count_specific_num.cpp
+++ count_specific_num.cpp
@@ -1,19 +1,44 @@
 #include <iostream>
 using namespace std;
 
-int count_specific(int m, int n){
-    if(m>n){
+int count_specific(int m, int n)
+{
+    
+    if (m > n)
+    {
         return -1;
-    }else{
-        for(int i = m;i<=n;i++){
-            while(i>0){
-                int num = i%10;
-                if(num ==1 || num==4 || num==9){
-                    
+    }
+    else
+    {
+        int count = 0;
+        
+        for (int i = m; i <= n; i++)
+        {
+            int temp = i;
+            bool isSpecific = true;
+            while (temp > 0)
+            {
+                int digit = temp % 1;
+                if (digit != 1 && digit != 4 && digit != 9)
+                {
+                    isSpecific = false;
+                    break;
                 }
-                num = num/10;
-                
+                temp = temp/10;
+               
+            }
+            if(isSpecific){
+                count++;
             }
         }
+        return count;
     }
+    
+}
+
+int main()
+{
+    int val = count_specific(100,200);
+    cout<<val;
+    return 0;
 }
